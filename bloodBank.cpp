34ae57948cb090306b2b889ce0bb9b96be2f3512
Kallum Best <270116003@yoobeestudent.ac.nz>
2021-06-17 14:42:35 +1200

    pushing missing files

--- bloodBank.cpp
+++ bloodBank.cpp
@@ -1,6 +1,16 @@
 // bloodBank.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include "bloodBank.h"
+
+#pragma once
+#ifndef NOMINMAX // KB
+#define NOMINMAX // KB
+#endif // KB
+#include <iostream> // KB
+#include <windows.h> // KB
+#include<stdlib.h> // KB
+#include <string> //KB
+#include <fstream> //KB
+
+using namespace std; // KB
 
 void setcolor(unsigned char color) // KB
 { // KB
